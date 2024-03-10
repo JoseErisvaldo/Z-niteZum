85824cd19583ec7485d02d3bdffbb2f0cffe45b8
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-10 16:44:22 -0300

    Feat: Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import RoutesApp from './Routes'
 
 function App() {
-  return <div className="App">Hello</div>
+  return (
+    <div className="App">
+      <RoutesApp />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Routes.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Header from "./Components/Header"
+
+export default function RoutesApp () {
+  return(
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} ></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
