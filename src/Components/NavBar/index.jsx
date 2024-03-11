670cddf1ca9e04a1e4b9f8ca86d6fbfa06d34104
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-11 19:57:39 -0300

    Feat: NavBar

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,27 @@
+import './style.css'
+
+
+export default function NavBar () {
+  return(
+    <div className="container-nave-bar">
+      <div>
+        Categorias
+      </div>
+      <div>
+        Ofertas do Dia
+      </div>
+      <div>
+        Celulares
+      </div>
+      <div>
+        Moveis
+      </div>
+      <div>
+        Eletronicos
+      </div>
+       <div>
+        Informatica
+      </div>
+    </div>
+  )
+}
