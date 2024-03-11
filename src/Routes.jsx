79f7c7a0eee5fc203777d2cb6788669f8f1ab799
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-11 19:59:13 -0300

    Feat: Route detailsitem

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
 import Header from "./Components/Header"
+import DetailesItem from "./Pages/DetalilsItem"
 
 export default function RoutesApp () {
   return(
@@ -8,6 +9,7 @@ export default function RoutesApp () {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>} ></Route>
+        <Route path="/detalhesitem/:id" element={<DetailesItem/>} />
       </Routes>
     </BrowserRouter>
   )
