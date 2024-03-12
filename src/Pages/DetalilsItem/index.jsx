952e6227aabedc8ef084e086f44ff3d81fa262c8
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-11 22:23:56 -0300

    Feat: DetailsItem

--- src/Pages/DetalilsItem/index.jsx
+++ src/Pages/DetalilsItem/index.jsx
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom"
 import CompoDetailesItem from "../../Components/CompoDetailsItem"
+import ItemQuestions from "../../Components/CompoDetailsItem/ItemQuestions"
 
 export default function DetailesItem () {
   return (
     <div>
-      <div><CompoDetailesItem/></div>
+      <div>
+        <CompoDetailesItem/>
+        <ItemQuestions/>
+      </div>
+      
     </div>
   )
 }
