4369187cfe9280aedec0df1ef4b13b0389181e4e
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-13 22:54:25 -0300

    Fix; Details

--- src/Components/CompoEmphasis/index.jsx
+++ src/Components/CompoEmphasis/index.jsx
@@ -18,7 +18,9 @@ export default function CompoEmphasis () {
       {itens.map((item) =>(
         <div className='card-emphasis-home' key={item.id}>
           <img src={item.thumbnail} />
+          <div>{item.title}</div>
           <div className='container-emphasis-price'>
+            
             <div className='card-emphasis-price'>R$ {item.original_price}</div>
             <div className='card-emphasis-discount'>R$ {item.price}</div>
           </div>
