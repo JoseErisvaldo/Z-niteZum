f417ec62e8c18a353b18fb6f4e02bd816a76ff65
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-13 22:54:50 -0300

    Feat: header cart

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,29 +1,50 @@
-import Logo from '../Logo'
-import './style.css'
+import Logo from '../Logo';
+import './style.css';
 import { CiSearch } from "react-icons/ci";
 import { VscAccount } from "react-icons/vsc";
 import { GoGear } from "react-icons/go";
 import { IoIosNotificationsOutline } from "react-icons/io";
+import { FiShoppingCart } from "react-icons/fi";
+import CompoCart from '../Cart';
+import { useState } from 'react';
+
 
 export default function Header () {
+  const [cart, setCart] = useState(false);
+
+  // Função para abrir/fechar o carrinho
+  function handleCart () {
+    // Alternar o estado do carrinho
+    setCart(!cart);
+  }
+
+  // Função para fechar o carrinho
+  function handleCloseCart() {
+    setCart(false);
+  }
+
   return(
     <div className="container-header">
       <div>
         <Logo/>
       </div>
       <div className='search-header'>
-        <input className='input-search' placeholder='Pequisar' />
+        <input className='input-search' placeholder='Pesquisar' />
         <div className='icon-input-search'>
           <CiSearch/>
         </div>
       </div>
+      {/* Passando a função handleCloseCart para CompoCart */}
+      <CompoCart list={cart} onClose={handleCloseCart} />
       <div>
         <ul className='dados-account'>
+          {/* Ao clicar no ícone do carrinho, abrir/fechar o carrinho */}
+          <li onClick={handleCart}><FiShoppingCart/></li>
           <li><IoIosNotificationsOutline/></li>
           <li><VscAccount/></li>
           <li><GoGear/></li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
